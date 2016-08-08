2c312ee103a90b7ecb934322e876fc10f1ba056e
Wolfgang Berger <w.berger@diamonddogs.cc>
2016-08-08 12:09:21 +0200

    first d3.js integration (not working when resetting the page)

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,30 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, ionicBootstrap, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+
 import {HomePage} from './pages/home/home';
 
-
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage: any = HomePage;
+  pages: Array<{title: string, component: any}>;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+
+    this.pages = [
+      { title: 'Home', component: HomePage }
+    ];
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
   }
 }
 
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,43 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 import {NavController} from 'ionic-angular';
+declare var d3: any;
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   constructor(private navCtrl: NavController) {
+  }
 
+  ngAfterViewInit() {
+    this.createChart();
+  }
+
+  createChart() {
+    var chart = d3.select("#chart").append("svg")
+      .attr("width", 100)
+      .attr("height", 100)
+      .append("g");
+
+    var rows = [
+      {x: 1, y: 1},
+      {x: 2, y: 2},
+      {x: 3, y: 3},
+    ];
+
+    var xScale = d3.scaleLinear()
+            .range([0, 100])
+            .domain([1, 3]);
+    var yScale = d3.scaleLinear()
+            .range([100, 0])
+            .domain([1, 3]);
+
+    chart.selectAll(".dot")
+      .data(rows)
+      .enter().append("circle")
+      .attr("class", "dot")
+      .attr("cx", (row) => { return xScale(row.x) })
+      .attr("cy", (row) => { return yScale(row.y) })
+      .attr("r", 3.5);
   }
 }
